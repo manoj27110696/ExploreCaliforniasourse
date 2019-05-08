b6040eb3770c3ff41f5a2630a5d4408eedc90066
manoj k <manoj0134@gmail.com>
2019-05-07 20:41:52 -0400

    initial changes

--- ExploreCaliforniasourse/Startup.cs
+++ ExploreCaliforniasourse/Startup.cs
@@ -5,30 +5,61 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExploreCaliforniasourse
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration=configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<FeatureToggles>( x=> new FeatureToggles{
+                DeveloperExceptions =configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
+            });
+            services.AddMvc();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env,
+            FeatureToggles features)
         {
-            if (env.IsDevelopment())
+
+            app.UseExceptionHandler("/error.html");
+
+            if (features.DeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.Use(async (context,next) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if(context.Request.Path.Value.Contains("invalid"))
+                {
+                    throw new Exception("ERROR!");
+                }
+                await next();
             });
+
+        
+            app.UseMvc(routes=>
+            {
+            routes.MapRoute("Default",
+                 "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseFileServer();
+
         }
     }
 }
